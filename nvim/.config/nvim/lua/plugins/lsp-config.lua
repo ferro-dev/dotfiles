ccdeaa35e5c60ca93b248a0d0d93dd489fe3b964
evan <evanscahill@gmail.com>
2026-03-25 18:31:57 -0300

    add git, nvim, and beets as stow packages
    - git/: adds .gitconfig (includes delta pager config)
    - nvim/: adds full neovim config — Lazy.nvim, LSP, completions,
      Telescope, neo-tree, treesitter, lualine, catppuccin, render-markdown,
      none-ls, and lazy-lock.json
    - beets/: adds config.yaml with hardcoded music path replaced by
      $MUSIC_DIR environment variable
    
    All three packages verified with stow --no-folding.
    
    Closes #3
    Closes #4
    Closes #5

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "angularls", "omnisharp" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.omnisharp.setup({
+				capabilities = capabilities,
+				cmd = { "dotnet", "/usr/bin/omnisharp/OmniSharp.dll" },
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", 
+    "MunifTanjim/nui.nvim"
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree reveal right<Enter>")
+    
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true
+        }
+      },
+      window = {
+        position = "right"
+      }
+    })
+  end
+}
